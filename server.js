696f6b4b1f91c673e9b402b8aac08729b854889b
heavsta <francoisleduc@ymail.com>
2020-06-20 21:23:49 +0200

    first commit, getting unexpected token issue

---
+++ server.js
@@ -0,0 +1,54 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Express to run server and routes
+const express = require('express');
+// Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser');
+
+/* Middleware*/
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//Here we are configuring express to use body-parser as middle-ware.
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+const port = 8000;
+
+// Spin up the server
+const server = app.listen(port, listening);
+// Callback to debug
+function listening () {
+    console.log('server running');
+    console.log(`running on localhost: ${port}`);
+}
+
+
+// Initialize all route with a callback function
+// Callback function to complete GET '/all'
+app.get ('/allData', (req, res) => {
+    res.send(projectData);
+    console.log(projectData);
+});
+
+const data = [];
+
+// Post Route
+app.post('/addData', (req, res) => {
+    const newEntry = {
+        temp: req.body.temp,
+        date: req.body.date,
+        content: req.body.content
+    }
+    projectData.push(newEntry);
+    res.send(projectData);
+    console.log(projectData);
+});
---
+++ website/app.js
@@ -0,0 +1,109 @@
+/* LOADING TIME IMAGES */
+
+function loadImage(id, targetId) {
+    var el = document.getElementById(id);
+    var targetEl = targetId ? document.getElementById(targetId) : el;
+    var imageToLoad;
+
+    if (el.dataset.image) {
+        imageToLoad = el.dataset.image;
+    } else if (typeof el.currentSrc === 'undefined') {
+        imageToLoad = el.src;
+    } else {
+        imageToLoad = el.currentSrc;
+    }
+
+    if (imageToLoad) {
+        var img = new Image();
+        img.src = imageToLoad;
+        img.onload = function() {
+            targetEl.classList.add('is-loaded')
+        };
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadImage('wallpaper');
+});
+
+
+
+
+/* UDACITY PROJECT */
+
+// Personal API Key for OpenWeatherMap API
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+let apiKey = '&appid=8d409df75ab1a756b861b595af9fae58';
+
+// create new date instance
+let d = new Date();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', performAction);
+/* Function called by event listener */
+function performAction(e) {
+    const zipCode = document.getElementById('zip').value;
+    const content = document.getElementById('content').value;
+    getWeatherData(baseURL, zipCode, apiKey)
+    .then(function(data) {
+        console.log(data);
+        postData('/addData', {
+            date: newDate,
+            temp: data.main.temp,
+            content: content
+        })
+    })
+    .then(
+        updateUI()
+    )
+
+}
+
+/* Function to GET Web API Data*/
+const getWeatherData = async(baseURL, zipCode, apiKey) => {
+    const res = await fetch(baseURL + zipCode + apiKey)
+    try {
+        const data = await res.json();
+        console.log(data);
+        return data;
+    }catch(error){
+console.log("error in GET Web API Data", error);
+    }
+}
+
+/* Function to POST data - send data to server */
+const postData = async(url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    }catch(error) {
+        console.log("error on postData", error);
+    }
+}
+
+/* UPDATE UI */
+
+const updateUI = async () => {
+    const request = await fetch('/allData')
+    try {
+        const allData = await request.json()
+        console.log(allData);
+        var index = allData.length - 1;
+        document.getElementById('date').innerHTML = 'Today is ' + allData[index].date;
+        document.getElementById('temp').innerHTML = 'The temperature is currently of ' + allData[index].temp;
+        document.getElementById('content').innerHTML = 'How you feel : ' + allData[index].content;
+    }catch(error) {
+        console.log("error in UI rendering", error);
+    }
+}
